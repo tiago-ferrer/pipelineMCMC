b0f2257ab874473b1e5b82e18b3df83159fb693f
Tiago Ferrer <ferrertiago@gmail.com>
2025-06-16 20:11:39 -0300

    ativando multithread

--- Eclipse/scripts/compile_windows.bat
+++ Eclipse/scripts/compile_windows.bat
@@ -1,6 +1,5 @@
 @echo off
 REM Compile 32-bit version
-cd scripts
 gcc -fopenmp -Wall -std=c11 -c -fPIC func.c -o func32.o -O3
 gcc -fopenmp -shared func32.o -o func32.dll -O3
 
@@ -14,4 +13,3 @@ gcc -fopenmp -Wall -std=c11 multithreadCheck.c -o multithreadCheck.exe
 echo Compilation complete!
 echo Running multithreadCheck to verify OpenMP functionality:
 multithreadCheck.exe
-cd ..
